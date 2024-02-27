d62c8d1675a906b3ee979930a7a6f820f6273eca
Demetrius Vissarion <demetrius.vissarion@gmail.com>
2024-02-27 18:01:20 +0000

    Get one and all books #2

--- app.js
+++ app.js
@@ -2,9 +2,26 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const { connectToDatabase } = require("./db/db.js");
-
-require("dotenv").config()
+require("dotenv").config();
 
 const app = express();
 
-connectToDatabase()
+const usersRouter = require("./routes/users");
+const booksRouter = require("./routes/books");
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use("/users", usersRouter);
+app.use("/books", booksRouter);
+
+const listenForRequests = () => {
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+        console.log("Now listening on port", port);
+    });
+};
+
+connectToDatabase().then(() => {
+    listenForRequests();
+});
