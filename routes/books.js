5a9331d5d7ff84cefd8b27b89b546a67d9e0f5d9
TomMazzag <itstomazz@gmail.com>
2024-02-28 11:55:14 +0000

    Fetch books adapted so that you can only choose categories given. Books filter BE done

--- routes/books.js
+++ routes/books.js
@@ -5,5 +5,6 @@ const BookController = require("../controllers/books");
 
 router.get("/all", BookController.getAllBooks);
 router.get("/find/:id", BookController.getBookByID);
+router.get("/filter", BookController.getBooksByCategories);
 
 module.exports = router;
