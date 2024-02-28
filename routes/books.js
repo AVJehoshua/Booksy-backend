b4f81f6982b1782c9285aae67d57c0656cc4bf04
Demetrius Vissarion <demetrius.vissarion@gmail.com>
2024-02-28 07:38:33 +0000

    fixed path typo

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const BookController = require("../controllers/book");
+const BookController = require("../controllers/books");
 
 router.get("/all", BookController.getAllBooks);
 router.get("/find/:id", BookController.getBookByID);
