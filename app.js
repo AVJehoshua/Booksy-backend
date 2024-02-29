9f5bc003f8efeefa9bc5fd6d612936c62918f052
ilhan <ilhanabdalle@gmail.com>
2024-02-29 12:52:41 +0000

    Create user function update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const { connectToDatabase } = require("./db/db.js");
 require("dotenv").config();
 const { Webhook } = require("svix");
-const User = require("./models/user.js");
+const UsersController = require("./controllers/users.js")
 
 const app = express();
 
@@ -20,7 +20,6 @@ app.use("/books", booksRouter);
 app.post("/api", bodyParser.raw({type: 'application/json'}), async (req, res) => {
     // Check if the 'Signing Secret' from the Clerk Dashboard was correctly provided
     const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
-    console.log(WEBHOOK_SECRET)
     if (!WEBHOOK_SECRET) {
         throw new Error("You need a WEBHOOK_SECRET in your .env");
     }
@@ -60,36 +59,22 @@ app.post("/api", bodyParser.raw({type: 'application/json'}), async (req, res) =>
       return;
     }
 
-    // Grab the ID and TYPE of the Webhook
-    const { id } = evt.data;
-    const eventType = evt.type;
+    // Below can be used for debugging: 
+      // Grab the ID and TYPE of the Webhook
+      const { id } = evt.data;
+      const eventType = evt.type;
 
-    console.log(`Webhook with an ID of ${id} and type of ${eventType}`);
-    // Console log the full payload to view
-    console.log("Webhook body:", evt.data);
+      console.log(`Webhook with an ID of ${id} and type of ${eventType}`);
+      // Console log the full payload to view
+      console.log("Webhook body:", evt.data);
 
     res.status(200).json({
       success: true,
       message: "Webhook received",
     });
 
-    console.log("I am evt data:", evt.data)
-
-    const first_name = evt.data.first_name;
-    const last_name = evt.data.last_name;
-    const email = evt.data.email_addresses[0].email_address;
-
-    const newUser = new User({ first_name, last_name, email });
-    await newUser
-        .save()
-        .then((user) => {
-            console.log("User created, id:", user._id.toString());
-            return res.status(201).json({ message: 'User created' });
-            })
-        .catch((err) => {
-            console.error(err);
-            return res.status(400).json({ message: "Something went wrong" });
-            });
+    // User is created in the database as a last step 
+    return await UsersController.create(evt)
 });
 
 
--- models/user.js
+++ models/user.js
@@ -4,6 +4,7 @@ const UserSchema = new mongoose.Schema({
     first_name: {type: String, required: true},
     last_name: {type: String, required: true},
     email: {type: String, required: true},
+    user_id:{type: String, required: true},
     address: {type: String},
     email_preferences: {type: Array},
     saved_items: {type: Array}
