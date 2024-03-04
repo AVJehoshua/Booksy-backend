ddaa1157489a477f0e1b8a51797df8b85a68feaf
TomMazzag <itstomazz@gmail.com>
2024-03-04 17:11:34 +0000

    date added

--- models/review.js
+++ models/review.js
@@ -14,6 +14,10 @@ const ReviewSchema = new mongoose.Schema({
     title: { type: String, required: true },
     content: { type: String, required: true },
     rating: { type: Number, min: 1, max: 5, required: true },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
 });
 
 const Review = mongoose.model("reviews", ReviewSchema);
