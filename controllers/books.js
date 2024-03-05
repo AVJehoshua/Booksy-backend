012a542313dfa4cbf64554d7eedcd1c153583b5b
Jehoshua <137894662+AVJehoshua@users.noreply.github.com>
2024-03-05 22:01:50 +0000

    added getBooksBySearch

--- controllers/books.js
+++ controllers/books.js
@@ -45,10 +45,35 @@ const getBooksByCategories = async (req, res) => {
     }
 };
 
+const getBooksBySearch = async (req, res) => {
+    
+    if (req.params.searchQuery !== " ")
+    console.log("backend author :", req.params.author)
+    try {
+        const authorBookResults = await Book.find({ $or: [
+
+            {author: { $regex: req.params.searchQuery, $options: 'i' }},
+            {title: { $regex: req.params.searchQuery, $options: 'i' }},
+        ]})
+
+        res.status(200).json({
+            message: `Fetched books with titles: ${authorBookResults}`,
+            book: authorBookResults
+        }); 
+        console.log(`backend author response sent: ${authorBookResults}`)
+    } catch (error) {
+        console.error('Error fetching books by author:', error);
+        res.status(500).json({ error: 'Internal server error' }); 
+    }
+};
+
+
 const BookController = {
     getAllBooks: getAllBooks,
     getBookByID: getBookByID,
     getBooksByCategories: getBooksByCategories,
+    getBooksBySearch: getBooksBySearch,
 };
 
 module.exports = BookController;
+
